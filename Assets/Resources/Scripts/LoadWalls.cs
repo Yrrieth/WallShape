a1d849aa7134278fb4abc3abaf2371b14d3e1aa3
Yrrieth <thierry.wen@laposte.net>
2020-06-04 17:39:50 +0200

    Modif fonction création des murs et Ajout suppression des murs

---
+++ Assets/Resources/Scripts/LoadWalls.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadWalls : MonoBehaviour
+{
+    public static GameObject[] wallModel;
+    public static List<GameObject> activeListWall;
+
+    public int speedWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadWallModel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Remove from the list if a wall doesn't exist
+        activeListWall.RemoveAll(GameObject => GameObject == null);
+        
+        moveWall(speedWall);
+    }
+
+    public void loadWallModel()
+    {
+        activeListWall = new List<GameObject>();
+
+        /* Charge tous les fichiers contenus dans le dossier à l'adresse suivante
+         * (Ces fichiers doivent être préalablement mis dans un dossier nommé "Resources/")
+         */
+        wallModel = Resources.LoadAll<GameObject>("Prefabs/Walls");
+    }
+
+    public void putWall(int wallNumber, float delayTime)
+    {
+        StartCoroutine(createWall(wallNumber, delayTime));
+    }
+
+    IEnumerator createWall(int wallNumber, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        GameObject currentWall = Instantiate(wallModel[wallNumber]);
+        currentWall.transform.position = new Vector3(0, 5, 50);
+        currentWall.tag = "Wall";
+        currentWall.GetComponent<Collider>().isTrigger = true;
+        if (currentWall.GetComponent<Rigidbody>() == null) {
+            currentWall.AddComponent<Rigidbody>();
+        }
+        currentWall.GetComponent<Rigidbody>().useGravity = false;
+        
+        activeListWall.Add(currentWall);
+    }
+
+    void moveWall(int movementSpeed)
+    {
+        foreach(var currentWall in activeListWall)
+        {
+            currentWall.transform.position += new Vector3(0, 0, -Time.deltaTime * movementSpeed);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Music_Dancin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Dancin : MonoBehaviour
+{
+    public LoadWalls loadWalls;
+    private AudioSource audioData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Wait until every wall asset have been loaded
+        if (loadWalls != null)
+        {
+            music();
+            audioData = GetComponent<AudioSource>();
+            audioData.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void music()
+    {
+        loadWalls.putWall(6, 0f);
+        loadWalls.putWall(2, 1f);
+        loadWalls.putWall(4, 1.5f);
+        loadWalls.putWall(0, 3f);
+        loadWalls.putWall(8, 4f);
+        loadWalls.putWall(10, 5.5f);
+        loadWalls.putWall(9, 6f);
+        loadWalls.putWall(1, 7f);
+        loadWalls.putWall(3, 8f);
+        loadWalls.putWall(5, 8.5f);
+        loadWalls.putWall(2, 10f);
+        loadWalls.putWall(1, 11f);
+        loadWalls.putWall(2, 12f);
+    }
+}
